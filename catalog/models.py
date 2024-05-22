8e5b69a9626059ec226f6e10f86b3e5c2a2211a0
ystass <stepanischevyy@mail.ru>
2024-05-22 19:10:34 +0400

    Создали базу данных в ручном режиме. Внесли изменения в настройки подключения. В приложении каталога создали модели Category и Product. Создали миграции для новых моделей и применили их.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Категория товаров",
+        help_text="Введите название категории",
+    )
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание", help_text="Опишите категорию"
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите название продукта",
+    )
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание", help_text="Опишите продукт"
+    )
+    image = models.ImageField(
+        upload_to="photo", blank=True, null=True, verbose_name="Загрузите фото продукта"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="products",
+    )
+    price = models.FloatField(verbose_name="Цена продукта в руб.")
+    created_at = models.DateField(
+        blank=True, null=True, verbose_name="Дата создания (записи в БД)"
+    )
+    updated_at = models.DateField(
+        blank=True, null=True, verbose_name="Дата последнего изменения (записи в БД)"
+    )
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "price", "created_at", "updated_at"]
+
+    def __str__(self):
+        return f"{self.name} {self.price}"
--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'homework_19',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty',
     }
 }
 
@@ -128,3 +130,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
