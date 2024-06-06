02480f08e980c75b5629ff27601becaf67fb1fe4
ystass <stepanischevyy@mail.ru>
2024-06-06 17:10:59 +0400

    Модифицировали вывод и обработку запросов: при открытии отдельной статьи увеличивается счетчик просмотров, выводится в список статей только те, которые имеют положительный признак публикации, при создании и редактировании динамически формируется slug name для заголовка, после успешного редактирования записи пользователь перенаправляется на просмотр этой статьи

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from blog.models import Article
 
@@ -9,19 +11,46 @@ class ArticleCreateView(CreateView):
     fields = ('title', 'content', 'preview', 'created_at', 'published', 'number_views',)
     success_url = reverse_lazy('blog:blog_list')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_art = form.save()
+            new_art.slug = slugify(new_art.title)
+            new_art.save()
+        return super().form_valid(form)
+
 
 class ArticleUpdateView(UpdateView):
     model = Article
     fields = ('title', 'content', 'preview', 'created_at', 'published', 'number_views',)
-    success_url = reverse_lazy('blog:blog_list')
+    #success_url = reverse_lazy('blog:blog_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_art = form.save()
+            new_art.slug = slugify(new_art.title)
+            new_art.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
 
 
 class ArticleListView(ListView):
     model = Article
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(published=True)
+        return queryset
 
 class ArticleDetailView(DetailView):
     model = Article
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.number_views += 1
+        self.object.save()
+        return self.object
 
 
 class ArticleDeleteView(DeleteView):
