1a2401933699c3998edc899295c1bcf802c2c396
ystass <stepanischevyy@mail.ru>
2024-06-18 20:38:27 +0400

    Реализован вывод в список продуктов информации об активной версии

--- catalog/views.py
+++ catalog/views.py
@@ -10,6 +10,22 @@ from catalog.models import Product, Version
 class ProductListView(ListView):
     model = Product
 
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        list_product = Product.objects.all()
+
+        for product in list_product:
+            version = Version.objects.filter(product=product)
+            activ_version = version.filter(is_active=True)
+            if activ_version:
+                product.active_version = activ_version.last().name
+                product.number_version = activ_version.last().version_number
+            else:
+                product.active_version = 'Нет активной версии'
+
+        context_data['object_list'] = list_product
+        return context_data
+
 
 class ProductDetailView(DetailView):
     model = Product
