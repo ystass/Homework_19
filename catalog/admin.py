fa3a03fd215d036d6af8e242650045dd09dad7c3
ystass <stepanischevyy@mail.ru>
2024-06-18 18:58:31 +0400

    Добавили в админку Версии

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from catalog.models import Product, Category
+from catalog.models import Product, Category, Version
+
 
 # admin.site.register(Product)
 # admin.site.register(Category)
@@ -16,3 +17,10 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category')
     list_filter = ('category',)
     search_fields = ('name', "description",)
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'version_number', 'name', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ("version_number",)
