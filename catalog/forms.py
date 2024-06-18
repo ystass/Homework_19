8a155f122cbbe1e66cc031b5b853e69e532fec87
ystass <stepanischevyy@mail.ru>
2024-06-18 19:56:51 +0400

    Добавили форму для версии VersionForm и стили StyleFormMixin

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,13 +1,21 @@
 from django import forms
 
-from catalog.models import Product
+from catalog.models import Product, Version
 
 
-class ProductForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            if not isinstance(field.widget, forms.CheckboxInput):
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-        #exclude = ('created_at', 'updated_at')
 
     def clean_name(self):
         cleaned_data = self.cleaned_data['name']
@@ -28,3 +36,9 @@ class ProductForm(forms.ModelForm):
                 raise forms.ValidationError("Такой продукт нельзя создать")
 
         return cleaned_data
+
+
+class VersionForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = '__all__'
