e99d61b0ae3ce9e5e63914ee9f70b981987a5534
ystass <stepanischevyy@mail.ru>
2024-06-25 11:57:37 +0400

    добавлена возможность редактирования профиля пользователя

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -8,3 +9,15 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
+
+
+class UserProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = ('email', 'avatar', 'phone', 'country')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['password'].widjet = forms.HiddenInput()
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
 
 
@@ -10,3 +10,12 @@ class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+
+class ProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
