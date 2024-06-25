45a5a6f16d5f12ea7ffe41f166201096c7daf20f
ystass <stepanischevyy@mail.ru>
2024-06-25 13:36:52 +0400

    исправление

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -7,12 +7,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         user = User.objects.create(email="admin@admin.com",
-                                   first_name='admin',
-                                   last_name='admin',
-                                   is_superuser=True,
-                                   is_staff=True,
-                                   is_active=True,
+                                   # first_name='admin',
+                                   # last_name='admin',
+                                   # is_superuser=True,
+                                   # is_staff=True,
+                                   # is_active=True,
                                    )
         user.set_password('qwe123asd456')
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
         user.save()
 
