0020104c83707ca015526ad5c4abb22b43bc9c59
ystass <stepanischevyy@mail.ru>
2024-06-26 12:54:34 +0400

    Revert "реализация регисрации через отправку письма"
    This reverts commit 83b70b971824e2a1d26b3d66d69348ab177d41e7.

--- users/urls.py
+++ users/urls.py
@@ -2,15 +2,14 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import ProfileView, UserCreateView, email_verification  # RegisterView,
+from users.views import RegisterView, ProfileView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', UserCreateView.as_view(), name='register'),
-    path('email-confirm/<str:token>/', email_verification, name='email-confirm'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile')
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,48 +1,15 @@
-import secrets
-
-from django.core.mail import send_mail
-from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
 from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
 
-from config.settings import EMAIL_HOST_USER
 
-
-class UserCreateView(CreateView):
+class RegisterView(CreateView):
     model = User
     form_class = UserRegisterForm
+    template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
-
-    def form_valid(self, form):
-        user = form.save()
-        user.is_activ = False
-        token = secrets.token_hex(16)
-        user.token = token
-        user.save()
-        host = self.request.get_host()
-        url = f'http://{host}/users/email.confirm/{token}/'
-        send_mail(
-            subject='Подтверждение почты',
-            message=f'Здравствуйте. Перейдите по ссылки для подтверждения почты {url}',
-            from_email=EMAIL_HOST_USER,
-            recipient_list=[user.email]
-        )
-        return super().form_valid(form)
-
-
-def email_verification(request, token):
-    user = get_object_or_404(User, token=token)
-    user.is_active = True
-    user.save()
-    return redirect(reverse('users:login'))
-# class RegisterView(CreateView):
-#     model = User
-#     form_class = UserRegisterForm
-#     template_name = 'users/register.html'
-#     success_url = reverse_lazy('users:login')
 
 
 class ProfileView(UpdateView):
