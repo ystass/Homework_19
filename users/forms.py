4333072913e680b36cb8719277301e7f90ad015b
ystass <stepanischevyy@mail.ru>
2024-06-27 12:13:57 +0400

    корректировки

--- users/forms.py
+++ users/forms.py
@@ -1,17 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
+from catalog.forms import StyleFormMixin
 from users.models import User
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
 
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
 
 
-class UserProfileForm(UserChangeForm):
+class UserProfileForm(StyleFormMixin, UserChangeForm):
 
     class Meta:
         model = User
--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -6,13 +6,7 @@ from users.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        user = User.objects.create(email="admin@admin.com",
-                                   # first_name='admin',
-                                   # last_name='admin',
-                                   # is_superuser=True,
-                                   # is_staff=True,
-                                   # is_active=True,
-                                   )
+        user = User.objects.create(email="admin@admin.com",)
         user.set_password('qwe123asd456')
         user.is_active = True
         user.is_staff = True
