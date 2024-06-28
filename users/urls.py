2d67d06b52e9cac8661e64bdce507912d8f65ee0
ystass <stepanischevyy@mail.ru>
2024-06-28 09:19:38 +0400

    Реализация восстановления на автоматически сгенерированный пароль

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import ProfileView, UserCreateView, email_verification  # RegisterView,
+from users.views import ProfileView, UserCreateView, email_verification, reset_password  # RegisterView,
 
 app_name = UsersConfig.name
 
@@ -11,6 +11,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', UserCreateView.as_view(), name='register'),
     path('email-confirm/<str:token>/', email_verification, name='email-confirm'),
+    path('reset_password/', reset_password, name='reset_password'),
     path('profile/', ProfileView.as_view(), name='profile')
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 import secrets
+import random
+import string
 
 from django.core.mail import send_mail
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
@@ -38,6 +40,34 @@ def email_verification(request, token):
     user.is_active = True
     user.save()
     return redirect(reverse('users:login'))
+
+
+def reset_password(request):
+    context = {
+        'success_message': 'Пароль успешно сброшен. Новый пароль был отправлен на адрес Вашей электронной почты.',
+    }
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        user = get_object_or_404(User, email=email)
+        characters = string.ascii_letters + string.digits
+        characters_list = list(characters)
+        random.shuffle(characters_list)
+        password = ''.join(characters_list[:10])
+
+        # user.set_password(make_password(password))
+        user.set_password(password)
+        user.save()
+
+        send_mail(
+            subject='Восстановление пароля',
+            message=f'Вот новый пароль: {password}',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+
+        return render(request, 'users/reset_password.html', context)
+
+    return render(request, 'users/reset_password.html')
 # class RegisterView(CreateView):
 #     model = User
 #     form_class = UserRegisterForm
