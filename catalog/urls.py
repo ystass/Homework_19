93e092cbdd33b32b0937911cb461486d34821278
ystass <stepanischevyy@mail.ru>
2024-07-08 12:52:05 +0400

    кеширование

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
     ProductDeleteView, toggle_in_stock  # home,contacts
@@ -12,7 +14,7 @@ urlpatterns = [
     path('catalog/<int:pk>/update/', ProductUpdateView.as_view(), name='catalog_update'),
     path('catalog/<int:pk>/delete/', ProductDeleteView.as_view(), name='catalog_delete'),
     path('', ProductListView.as_view(), name='product'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='one_product'),
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='one_product'),
     path('activity/<int:pk>/delete/', toggle_in_stock, name='toggle_in_stock')
 ]
 
